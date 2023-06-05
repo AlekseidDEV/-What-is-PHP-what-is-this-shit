9348d62f854d2907fc6aaa8955cbc5b43a07d7e0
Lesha <lesha.rudiko@gmail.com>
2023-06-05 20:39:43 +0300

    start

---
+++ index.php
@@ -0,0 +1,34 @@
+<meta charset="utf-8">
+    <?php
+        mb_internal_encoding('UTF-8');
+        error_reporting(E_ALL);
+        ini_set('display errors' , 'on');
+        echo 'привет мир!';
+        include 'code.php';
+    ?>
+
+    <!-- вывод на экран -->
+
+
+
+    <!-- <php
+        mb_internal_encoding('UTF-8');
+        echo 'привет мир!';
+    > -->
+    <!-- указание кодировки для коректного отображения -->
+
+    <!-- <php 
+        error_reporting(E_ALL);
+        ini_set('display errors' , 'on')
+    > -->
+
+    <!-- вывод всех ошибок (фатал, варнинг, нотисай) -->
+
+
+    <!-- <php 
+        error_reporting(0);
+        ini_set('display errors' , 'off')
+    > -->
+
+    <!-- это настройка для выключения на выложенном сайте на хостинг -->
+
